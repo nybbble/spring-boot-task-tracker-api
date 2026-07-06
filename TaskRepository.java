a4ccedf43b277dfd6abe91dd1c63c32c7b38b314
nybbble <metinbbusra@gmail.com>
2026-07-07 00:17:42 +0300

    Delete unnecessary files.

--- TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.taskmanagement.repository;
-
-import com.example.taskmanagement.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
