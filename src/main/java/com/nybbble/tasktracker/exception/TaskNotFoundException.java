f206b6b401e2e5d9dce9e9c672bcbff43ca69969
nybbble <metinbbusra@gmail.com>
2026-07-07 00:12:47 +0300

    Add TaskNotFoundException for missing tasks

---
+++ src/main/java/com/nybbble/tasktracker/exception/TaskNotFoundException.java
@@ -0,0 +1,8 @@
+package com.nybbble.tasktracker.exception;
+
+public class TaskNotFoundException extends RuntimeException {
+
+    public TaskNotFoundException(Long id) {
+        super("Task not found with id: " + id);
+    }
+}
